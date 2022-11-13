d76058b98cf31985dd89e3d3f6888aca040de709
KwonSe-Jin <sejin6849@gmail.com>
2022-11-13 23:28:24 +0900

    11-13
    공 중력 추가

--- Ball.py
+++ Ball.py
@@ -4,19 +4,25 @@ import Move
 from pico2d import *
 import play_state
 import Background1
-import math
+from math import *
 import game_framework
 import title_state
+
+
 class Ball:
     def __init__(self):
+
        self.image = load_image('ball.png')
-       self.x = 400
-       self.y = 500
        self.frame = 0
-       self.b_speedx = 10
-       self.b_speedy = 10
+       self.x = 150
+       self.y = 300
+       self.to_x = 3
+       self.to_y = -6
+       # self.init_spd_x = 10
+       self.init_spd_y = 30
        self.font = load_font('ENCR10B.TTF', 50)
        self.point = 0
+
 
     def get_bb(self):
         return self.x - 25, self.y - 25, self.x + 25, self.y + 25
@@ -27,33 +33,32 @@ class Ball:
 
     def update(self):
         self.frame = (self.frame + 1) % 5
-        self.x += self.b_speedx
-        self.y += self.b_speedy
-        if self.x <= 50:
-            self.b_speedx = -self.b_speedx
-            self.x = 50
-        elif self.x >= 750:
-            self.b_speedx = -self.b_speedx
-            self.x = 750
-        if self.y <= 50:
-            self.b_speedy = -self.b_speedy
-            self.y = 50
-        elif self.y >= 550:
-            self.b_speedy = -self.b_speedy
-            self.y = 550
-        if  self.x > 400 and self.y <= 50:
-            print("bottom")
-            self.point += 1
+        if self.x <= 50 or self.x >= 750:
+            self.to_x = -self.to_x
+        if self.y < 50:
+            self.to_y = self.init_spd_y
+            self.init_spd_y -= 2
+        else:
+            self.to_y -= 0.5
+        if self.y >= 580:
+            self.to_y = -self.to_y
+        self.x += self.to_x
+        self.y += self.to_y
+        # if self.x > 400 and self.y < 50:
+        #     print("bottom")
+        #     self.point += 1
 
         if (collision.collide(self, play_state.pikachu)):
-            self.b_speedx = -self.b_speedx
-            self.b_speedy = -self.b_speedy
+            self.to_x = -self.to_x
+            self.to_y = self.init_spd_y
+            self.init_spd_y = 16
         if (collision.collide(self, play_state.pikachu2)):
-            self.b_speedx = -self.b_speedx
-            self.b_speedy = -self.b_speedy
-        if (collision.collide(self, play_state.net)):
-            self.b_speedx = -self.b_speedx
-            self.b_speedy = -self.b_speedy
+            self.to_x = -self.to_x
+            self.to_y = self.init_spd_y
+            self.init_spd_y = 16
+        # if (collision.collide(self, play_state.net)):
+        #     self.init_spd_x = -self.init_spd_x
+        #     self.init_spd_y = -self.init_spd_y
 
         if self.point == 5:
             game_framework.change_state(title_state)
